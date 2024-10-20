c4e08690d320d3301e668a6b4188bf10a2023255
manuelcely <fernandocly18@gmail.com>
2024-10-19 23:55:46 -0500

    How to use @GeneratedValue in Atribute on a Entity Author

--- src/main/java/com/celyngcoding/jpa/models/Author.java
+++ src/main/java/com/celyngcoding/jpa/models/Author.java
@@ -1,9 +1,7 @@
 package com.celyngcoding.jpa.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,7 +11,26 @@ import lombok.NoArgsConstructor;
 public class Author {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue/*( // HAVE diferent types, default is AUTO
+            //strategy = GenerationType.SEQUENCE,
+            //generator = "author_sequence"
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
+    )*/
+    /*
+    @SequenceGenerator( // the sequence taht we want to use
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    ) */
+    /*
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )*/
     private Integer id;
     private String firstName;
     private String lastName;
